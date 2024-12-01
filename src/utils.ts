b49b1ade75fc034347bd7e2572c3a7ce9b4b7dc4
Alyxia Sother <alyxia@riseup.net>
2024-12-01 14:58:26 +0100

    [Meta] Debug logs & Node downgrade

--- src/utils.ts
+++ src/utils.ts
@@ -208,6 +208,7 @@ export function getJSON<T>(endpoint: string, headers: Record<string, string>): P
                 data += piece;
             });
             res.on('end', (): void => {
+                console.log(data);
                 try {
                     resolve(JSON.parse(data));
                 } catch (e) {
